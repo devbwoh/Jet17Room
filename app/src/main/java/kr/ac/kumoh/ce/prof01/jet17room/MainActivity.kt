f9f184fa5dd032c2db2e6c382dd5b111c604ee96
devbwoh <bwoh@outlook.kr>
2024-01-28 04:34:41 +0900

    ✨ 노래 추가

--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet17room/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet17room/MainActivity.kt
@@ -3,16 +3,25 @@ package kr.ac.kumoh.ce.prof01.jet17room
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kr.ac.kumoh.ce.prof01.jet17room.ui.theme.Jet17RoomTheme
 
@@ -33,16 +42,25 @@ fun MainScreen() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        SongList()
+        Scaffold(
+            floatingActionButton = {
+                SongAddButton()
+            }
+        ) {
+            SongList(it)
+        }
     }
 }
 
 @Composable
-fun SongList() {
+fun SongList(paddingValues: PaddingValues) {
     val viewModel: SongViewModel = viewModel()
     val songs by viewModel.songs.collectAsState()
 
-    LazyColumn {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        contentPadding = paddingValues,
+    ) {
         items(
             items = songs,
             key = {
@@ -51,5 +69,22 @@ fun SongList() {
         ) {
             Text(it.toString())
         }
+    }
+}
+
+@Composable
+fun SongAddButton() {
+    val viewModel: SongViewModel = viewModel()
+
+    FloatingActionButton(
+        onClick = {
+            viewModel.add("아무노래", "지코")
+        },
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Icon(
+            Icons.Filled.Add,
+            "추가 버튼"
+        )
     }
 }

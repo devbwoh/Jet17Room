a7a9249095709837a98f24b1505addaabc768cb9
devbwoh <bwoh@outlook.kr>
2024-01-28 04:37:30 +0900

    🎴 Card 사용

--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet17room/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet17room/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -22,6 +27,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kr.ac.kumoh.ce.prof01.jet17room.ui.theme.Jet17RoomTheme
 
@@ -67,9 +73,41 @@ fun SongList(paddingValues: PaddingValues) {
                 it.id
             }
         ) {
-            Text(it.toString())
+            SongItem(it)
         }
     }
+}
+
+@Composable
+fun SongItem(song: Song) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(8.dp),
+    ) {
+        Column(
+            Modifier.padding(8.dp)
+        ) {
+            SongTitle(song.title)
+            SingerName(song.singer)
+        }
+    }
+}
+
+@Composable
+fun SongTitle(title: String) {
+    Text(
+        text = title,
+        modifier = Modifier.fillMaxWidth(),
+        fontSize = 20.sp,
+        lineHeight = 25.sp,
+    )
+}
+
+@Composable
+fun SingerName(name: String) {
+    Text(name)
 }
 
 @Composable
